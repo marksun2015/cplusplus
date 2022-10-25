5a16dc5f1067a77d20dda08ea70c995c142d8896
Mark Sun <mark@weintek.com>
2022-10-25 10:51:46 +0800

    Modify code

--- practice/algorithm.cpp
+++ practice/algorithm.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main() {
     vector<int> nums = {1, 2, 3, 4, 5};
+
     auto it1 = find_if(nums.begin(), nums.end(), [] (int x) { return x % 2 == 0;} );
     if (it1 != nums.end())
         std::cout << "it1 " << *it1 << std::endl;
@@ -18,5 +19,12 @@ int main() {
         std::cout << "it2 " << *it2 << std::endl;
     } else {
         std::cout << "not find " << std::endl;
+    }
+
+    if (binary_search(nums.begin(), nums.end(), 5)) {
+        cout << "Element found" << endl;
+    }
+    else {
+        cout << "Element not found" << endl;
     }
 }
