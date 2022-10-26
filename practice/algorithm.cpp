0796d7cc25137a750c34fbf0d989310d9d215226
Mark Sun <mark@weintek.com>
2022-10-26 12:03:52 +0800

    Modify code

--- practice/algorithm.cpp
+++ practice/algorithm.cpp
@@ -7,19 +7,26 @@
 using namespace std;
 
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5};
+    vector<int> nums = {1, 5, 4, 2, 3};
 
-    auto it1 = find_if(nums.begin(), nums.end(), [] (int x) { return x % 2 == 0;} );
+    auto it1 = std::find_if(nums.begin(), nums.end(), [] (int x) { return x % 2 == 0;} );
     if (it1 != nums.end())
         std::cout << "it1 " << *it1 << std::endl;
     
-    auto it2 = find(nums.begin(), nums.end(), 4);
+    auto it2 = std::find(nums.begin(), nums.end(), 4);
     if (it2 != nums.end()) {
         std::cout << "find ";
         std::cout << "it2 " << *it2 << std::endl;
     } else {
         std::cout << "not find " << std::endl;
     }
+    
+    auto it3 = std::min_element(nums.begin(), nums.end());
+    std::cout << "min element " << *it3 << std::endl;
+
+    std::sort(nums.begin(), nums.end());    
+    for (auto n : nums) 
+        std::cout << "n: " << n << std::endl;
 
     if (binary_search(nums.begin(), nums.end(), 5)) {
         cout << "Element found" << endl;
