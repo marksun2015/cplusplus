3b12553870c3f2b021ea072df5b2317f19597890
Mark Sun <mark@weintek.com>
2022-10-30 17:31:09 +0800

    Modify code

---
+++ practice/operator1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Power {
+    int kick;
+    int punch;
+    public:
+    Power(int kick = 0, int punch = 0) {
+        this->kick = kick; 
+        this->punch = punch;
+    }
+    void show();
+    Power& operator<<(int n);
+    friend Power operator+(Power op1, Power op2);
+};
+
+void Power::show() {
+    cout << "kick=" << kick << ',' << "punch=" << punch << endl;
+}
+
+Power& Power::operator<<(int n) {
+    kick += n;
+    punch += n;
+    return *this;
+}
+
+Power operator+(Power op1, Power op2) {
+    Power tmp;
+    tmp.kick = op1.kick + op2.kick;
+    tmp.punch = op1.punch + op2.punch;
+    return tmp;
+}
+
+int main() {
+    //Power a(1, 2);
+    //a << 3 << 5 << 6;
+
+    Power a(3, 5), b(4, 6), c;
+    c = a + b;
+    a.show();
+    b.show();
+    c.show();
+}
