49488a1fa746a79eac33940ee24237dd61d23eef
Mark Sun <mark@weintek.com>
2022-11-30 10:25:28 +0800

    add code

--- practice/vector_string.cpp
+++ practice/vector_string.cpp
@@ -28,4 +28,15 @@ int main() {
 
     for (auto elem : ivec1)
         std::cout << elem << std::endl;
+
+
+    string str1 = "abc";
+    string str2 = "efg";
+    string str3 = "hij";
+    string str4;
+    str1.append(str2);
+    str4 = str2 + str3; 
+    std::cout << str1 << std::endl;
+    std::cout << str4 << std::endl;
+
 }
