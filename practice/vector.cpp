0a52ad5a3e3327f14d20ed752719038b86246281
Mark Sun <mark@weintek.com>
2022-11-30 16:00:30 +0800

    Modify code

--- practice/vector.cpp
+++ practice/vector.cpp
@@ -11,28 +11,43 @@ int main() {
     vector<int> ivec2[10];   // 宣告10個vector，每一個都可以存int。
     
     vector<int> ivec3 = {1,2,3,4,5,6,7,8,9,10}; //記下來,assign方式  
+    ivec3[8] = 8;
+    ivec3[9] = 8;
+    
     vector<string> ivec4 = {"aaa","bbb"}; //記下來,assign方式  
 
     ivec1[0] = 1;
     ivec1[1] = 2;
     ivec1[2] = 3;
     ivec1.push_back(4);  // 宣告1個vector，裡面有10個int空間。
-    
-    ivec3[0] = 8;
-    ivec3[1] = 8;
-    ivec3[2] = 8;
 
+    vector<int> ivec5(ivec3.begin(), ivec3.end()); //記下來,assign方式
+
+    std::cout << "---1-------------------" << std::endl;
+    for (auto &elem : ivec1) {
+        std::cout << &elem << std::endl;
+        if (&elem == &ivec1[0])
+            elem = 9;
+    }
+    //std::cout << "begin " << ivec1.begin() << std::endl;
+
+    for(int i = 0; i < ivec1.size(); i++)
+        std::cout << ivec1[i] << std::endl;
+    
     for (auto elem : ivec1)
         std::cout << elem << std::endl;
 
+    std::cout << "---2-------------------" << std::endl;
     std::cout << std::endl;
     for (auto &elem : ivec1)
         std::cout << elem << std::endl;
     
+    std::cout << "---3-------------------" << std::endl;
     std::cout << std::endl;
     for (auto &elem : ivec1)
         elem = 5;
 
-    for (auto elem : ivec1)
+    std::cout << "---4-------------------" << std::endl;
+    for (auto elem : ivec5)
         std::cout << elem << std::endl;
 }
